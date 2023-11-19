e75e5d34ae2497cda39ea24a7248a33e8eb72b76
Matthew Baer <matt.baer77@gmail.com>
2023-11-19 18:48:12 -0500

    insertion sort implemented

--- insertion.js
+++ insertion.js
@@ -1,3 +1,36 @@
-function insertionSort() {}
+function insertionSort(arr) {
+
+    const sortedArr = []
+
+    for (let i=0; i < arr.length; i++){
+
+        if(sortedArr.length === 0){
+
+            sortedArr.push(arr[i]);
+
+        } else {
+
+            for(let j = sortedArr.length - 1; j >= 0; j-- ){
+
+                if(arr[i] > sortedArr[j]){
+    
+                    sortedArr.splice(j+1, 0, arr[i])
+                    break
+    
+                } else if (j === 0){
+
+                    sortedArr.unshift(arr[i])
+
+                }
+    
+            }
+
+        }
+
+    }
+
+    return sortedArr
+
+}
 
 module.exports = insertionSort;
