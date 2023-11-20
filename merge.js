f39a0963511935d07327c6e7cc7c756c24cbcbc7
Matthew Baer <matt.baer77@gmail.com>
2023-11-19 19:11:27 -0500

    merge and mergeSort completed - iterative

--- merge.js
+++ merge.js
@@ -1,5 +1,62 @@
-function merge() {}
+function merge(arr1, arr2) {
 
-function mergeSort() {}
+    const mergedArr = []
+
+    let i = 0;
+    let j = 0;
+
+    while (i < arr1.length && j < arr2.length) {
+
+        if(arr1[i] < arr2[j]) {
+
+            mergedArr.push(arr1[i]);
+            i++;
+
+        } else {
+
+            mergedArr.push(arr2[j]);
+            j++;
+
+        }
+
+    }
+
+    while(i < arr1.length){
+        mergedArr.push(arr1[i])
+        i++;
+    }
+
+    while(j < arr2.length){
+        mergedArr.push(arr2[j])
+        j++;
+    }
+
+    return mergedArr
+
+}
+
+function mergeSort(arr) {
+
+    // NOTE TO SELF - CHECK NOTES FOR RECURSIVE VERSION.
+
+    const allArrs = [];
+
+    for(value of arr){
+
+        allArrs.push([value])
+
+    }
+
+    let result = [];
+
+    for(eachArr of allArrs){
+
+        result = merge(result, eachArr);
+
+    }
+
+    return result
+
+}
 
 module.exports = { merge, mergeSort};
